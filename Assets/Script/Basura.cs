a667d3a54cdd57ade4c1774bf8ddc6e997979e94
Luis Solano <solanoluigi@gmail.com>
2023-06-24 18:45:09 +0200

    modelos

--- Assets/Script/Basura.cs
+++ Assets/Script/Basura.cs
@@ -6,12 +6,14 @@ public class Basura : MonoBehaviour
 {
     Rigidbody body;
     float direction;
+    public float min=0;
+    public float max=1;
     void Start()
     {
 
         body=GetComponent<Rigidbody>();
         direction=Random.Range(5f,10f)*Mathf.Sign(transform.position.z)*-1;
-        
+        body.angularVelocity= new Vector3(Random.Range(min,max),Random.Range(min,max),Random.Range(min,max));
     }
     void Update() {
         body.velocity=new Vector3(body.velocity.x,body.velocity.y,direction);
